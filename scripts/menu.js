572369922b1d964c70063061b06fcfc3c2125014
Kavy Porwal <159620541+IamKavy47@users.noreply.github.com>
2025-09-19 05:00:56 +0000

    initial 2 logo

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,21 +1,21 @@
 // GSAP Setup
-const overlay = document.querySelector("[data-overlay]");
-const openButton = document.querySelector(".navbar-right .nav-link");
-const closeButton = document.querySelector("[data-close]");
+const overlay = document.querySelector("[data-overlay]")
+const openButton = document.querySelector(".navbar-right .nav-link")
+const closeButton = document.querySelector("[data-close]")
 
 // Open Menu
 openButton.addEventListener("click", (e) => {
-  e.preventDefault();
+  e.preventDefault()
 
   gsap.to(overlay, {
     clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
     duration: 0.6,
     ease: "power2.inOut",
     onStart: () => {
-      overlay.style.pointerEvents = "auto"; // Enable interaction during animation
+      overlay.style.pointerEvents = "auto" // Enable interaction during animation
     },
-  });
-});
+  })
+})
 // Close Menu
 closeButton.addEventListener("click", () => {
   gsap.to(overlay, {
@@ -23,18 +23,18 @@ closeButton.addEventListener("click", () => {
     duration: 0.6,
     ease: "power2.inOut",
     onComplete: () => {
-      overlay.style.pointerEvents = "none"; // Disable interaction after animation
+      overlay.style.pointerEvents = "none" // Disable interaction after animation
     },
-  });
-});
+  })
+})
 
 document.addEventListener("DOMContentLoaded", () => {
-  const links = document.querySelectorAll(".menu-links-new .text");
-  const currentPage = window.location.pathname.split("/").pop();
+  const links = document.querySelectorAll(".menu-links-new .text")
+  const currentPage = window.location.pathname.split("/").pop()
 
-  links.forEach(link => {
+  links.forEach((link) => {
     if (link.getAttribute("href") === currentPage) {
-      link.classList.add("active");
+      link.classList.add("active")
     }
-  });
-});
+  })
+})
