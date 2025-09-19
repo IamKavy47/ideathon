99287b1a007d647171cf63e709aa586cf50ce801
Kavy Porwal <159620541+IamKavy47@users.noreply.github.com>
2025-09-19 21:44:39 +0000

    date

--- scripts/flicker.js
+++ scripts/flicker.js
@@ -1,6 +1,6 @@
 const flickerElement = document.getElementById("flicker");
 const text1 = "Innovate. Ideate. Elevate.<br>Join the College-Exclusive<br>Ideathon '25";
-const text2 = `<span class="large">ThinkUp '25</span><br>Sep 15-18 '25`;
+const text2 = `<span class="large">ThinkUp '25</span><br>Sep 20-Oct 11 '25`;
 
 setInterval(() => {
   flickerElement.innerHTML = flickerElement.innerHTML === text1 ? text2 : text1;
